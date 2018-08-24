e87db3998e400a3d6cc5331cb637c1aa01872c6d
juliosfro <juliosfro@gmail.com>
2018-08-24 12:12:03 -0300

    Desafio requisição AJAX com JSON e PHP.

--- recebe.php
+++ recebe.php
@@ -1,32 +1,33 @@
 <?php
+// Recebendo os dados por método POST.
   $dados = $_POST['envio'];
-// conversão de array de objetos json para array de objetos php.
 
+// Conversão de array de objetos json para array de objetos php.
   $pessoas = array();
   foreach ($dados as $value){
     array_push($pessoas, json_decode($value));
   }
 
-// filtrando apenas quem mora na china para dentro do array de chineses.
+// Filtrando apenas quem mora na china para dentro do array de chineses.
   $chineses = array_filter($pessoas, function ($p) {
     return $p->pais == 'China'; 
   });
 
-// filtrando apenas as mulheres chinesas
+// Filtrando apenas as mulheres chinesas.
   $mulheres = array_filter($chineses, function ($m) {
     return $m->genero == 'F'; 
   });
 
-// filtrando o salário das mulheres chinesas
+// Mapeando apenas o salário das mulheres chinesas para um novo array.
   $salariosMulheresChinesas = array_map(function ($x) { return $x->salario; }, $mulheres);
 
-// Filtrando o menor salário das mulheres chinesas
+// Filtrando o menor salário das mulheres chinesas.
   $menorSalario = array();
   foreach ($mulheres as $value) {
     if ($value->salario == min($salariosMulheresChinesas))
       array_push($menorSalario, $value);
   }
-// retornando os dados para o ajax.
+// Retornando os dados para o ajax.
   $retorno = json_encode($menorSalario);
   print $retorno;
  ?>
